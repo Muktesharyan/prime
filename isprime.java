178cab2f1d62d6b5b92980731376a960c7547047
Muktesharyan <58796106+Muktesharyan@users.noreply.github.com>
2019-12-12 07:44:04 +0530

    First commit

---
+++ isprime.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+class isprime
+{
+	static boolean prime(int n)
+	{
+		int i;
+		if(n<=1) return false;
+		if(n<4) return true;
+		for(i=3;i<n;i=i+2)
+		{
+			if(n%i==0)
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+	public static void main(String args[]) 
+{		
+		Scanner kb=new Scanner(System.in);
+		System.out.println("Enter the number:");
+		int k=kb.nextInt();
+		
+		if(isprime.prime(k))
+		{
+			System.out.println("Success");
+		}
+		if(isprime.prime(k)==false)
+		{
+			System.out.println("Failure");
+		}	 
+	}
+}
